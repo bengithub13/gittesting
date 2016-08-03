678e5992da5cf0669b61fd6d85d6e4a5fd6fc38e
bengithub13 <bne.poon@gmail.com>
2016-08-02 23:12:26 -0400

    Master branch: MasterClass1

---
+++ src/gittesting/Masterclass1.java
@@ -0,0 +1,9 @@
+package gittesting;
+
+public class Masterclass1 {
+//Master's 1st class.  to create git repository in command line
+//git init 
+//git add .
+// go to git persepctive and add local repository	
+	
+}
